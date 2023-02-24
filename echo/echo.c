8f9d3dcf89aa8e380ea2454f579ca923c3b1310e
Mauro Bardetta <mbardett@e4r12p1.42roma.it>
2023-02-24 13:46:37 +0100

    echo migliorato ma matteo con la scusa della fame mi ostacola il lavoro

--- echo/echo.c
+++ echo/echo.c
@@ -9,7 +9,7 @@
 //ALLO STATO ATTUALE NON COPIA argv nella matrice args(o quanto minimo li stampa vuoti),
 // inizializzati male o non terminati?
 
-int	put_str(char *str, int fd)
+int	put_str_fd(char *str, int fd)
 {
 	int	i;
 
@@ -25,10 +25,13 @@ int	str_add(char *str1, const char *str2)
 {
 	int	i;
 
-	i = -1;
-	while(str2[++i])
+	i = 0;
+	while(str2[i])
+	{
 		str1[i] = str2[i];
-	str1[i] = '\0';
+		i++;
+	}
+	//str1[i] = '\0';
 	//printf("%s\n", str1);
 	return (0);
 }
@@ -59,13 +62,15 @@ int	echo_aux(int argc, char **argv, t_command *cmd)
 	n = 0;
 	while(n < argc - 1)
 	{
-		str_add(&cmd->args[0][n], argv[n + 1]);
-		put_str(cmd->args[n], 1);
-		write(1, "dioporco\n", 10);
-		printf("%d\n", n);
+		str_add(cmd->args[n], argv[n + 1]);
+		//put_str_fd(&cmd->args[0][n], 1);
+		//write(1, "dioporco\n", 10);
+		//printf("%d\n", n);
+		//printf("%ld\n", strlen(cmd->args[n]));
 		n++;
 	}
-	//str_add(cmd->args[n], "\0");
+	//str_add(&cmd->args[0][n], "\0");
+	//printf("%ld\n", strlen(&cmd->args[0][n]));
 	return (0);
 }
 
@@ -79,68 +84,25 @@ int	main(int argc,char **argv)
 	cmd = (t_command *)malloc(sizeof(t_command));
 	if (!cmd)
 		return (1);
-	write(1, "porcamadonna\n", 14);
 	i = 0;
 	n = 0;
 	to_ret = 0;
-	//put_str("culo", 1);
-	// put_str("culetto", 1);
-	// while (++n < argc)
-	// 	str_add(cmd->args[n - 1], &argv[0][n]);
-	// put_str("culone", 2);
-	// str_add(cmd->args[n], "\0");
 	echo_aux(argc, argv, cmd);
-	//write(1, "cioa stronzo\n", 14);
-	if (!ft_strncmp(cmd->args[0], "echo", 4))
+	if (!ft_strncmp(cmd->args[0], "echo", 4) && argc > 1)
 	{
-		put_str(cmd->args[0], 1);
-
-		if (!ft_strncmp(cmd->args[1], "-n", 2))
+		if (!ft_strncmp(cmd->args[1], "-n", 3))
 		{
-			put_str(cmd->args[2], 1);
+			put_str_fd(cmd->args[2], 1);
 			write(1, "\%", 2);
 			to_ret = 0;
 		}
 		else
-			put_str(cmd->args[1], 1);
-			write(1, "\n", 2);
+		{
+			put_str_fd(cmd->args[1], 1);
+			write(1, "\n", 1);
+		}
 	}
-	write(1, "cioa stronzo\n", 14);
+	if (argc == 1)
+		write(1, "\n", 1);
 	return (to_ret);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//s
