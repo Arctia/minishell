62d648db6c1cdbe5906b63b39a7c2c2dbb4de04f
Mauro Bardetta <mbardett@e4r11p1.42roma.it>
2023-02-25 19:24:44 +0100

    bisogna aggiungere un check per la stessa flag ripetuta, della serie -n -n -n -n, Jack dixit

--- echo/echo.c
+++ echo/echo.c
@@ -6,8 +6,21 @@
 // 	return (0);
 // }
 //
-//ALLO STATO ATTUALE NON COPIA argv nella matrice args(o quanto minimo li stampa vuoti),
-// inizializzati male o non terminati?
+//Verifica come si comporta con i caratteri speciali tipo e accentata etc
+//
+int	str_add(char *str1, const char *str2)
+{
+	int	i;
+
+	i = 0;
+	while(str2[i])
+	{
+		str1[i] = str2[i];
+		i++;
+	}
+	str1[i] = '\0';
+	return (0);
+}
 
 int	put_str_fd(char *str, int fd)
 {
@@ -21,20 +34,35 @@ int	put_str_fd(char *str, int fd)
 	}
 	return (i);
 }
-int	str_add(char *str1, const char *str2)
-{
-	int	i;
 
-	i = 0;
-	while(str2[i])
-	{
-		str1[i] = str2[i];
-		i++;
-	}
-	//str1[i] = '\0';
-	//printf("%s\n", str1);
-	return (0);
-}
+
+// int	str_add(char *str1, const char *str2)
+// {
+// 	int	i;
+
+// 	i = 0;
+// 	while(str2[i])
+// 	{
+// 		str1[i] = str2[i];
+// 		i++;
+// 	}
+// 	str1[i] = '\0';
+// 	return (0);
+// }
+
+// int	put_str_fd(char *str, int fd)
+// {
+// 	int	i;
+
+// 	i = 0;
+// 	//str_add(str, str);
+// 	while (str[i])
+// 	{
+// 		write (fd, &str[i], 1);
+// 		i++;
+// 	}
+// 	return (i);
+// }
 
 int	ft_strncmp(const char *s1, const char *s2, unsigned int n)
 {
@@ -53,56 +81,44 @@ int	ft_strncmp(const char *s1, const char *s2, unsigned int n)
 
 int	echo_aux(int argc, char **argv, t_command *cmd)
 {
-	//int	i;
 	int	n;
-	//t_command	*tmp;
-	//tmp = cmd;
 
-	//i = 0;
 	n = 0;
-	while(n < argc - 1)
+	while (n < argc - 1)
 	{
 		str_add(cmd->args[n], argv[n + 1]);
-		//put_str_fd(&cmd->args[0][n], 1);
-		//write(1, "dioporco\n", 10);
-		//printf("%d\n", n);
-		//printf("%ld\n", strlen(cmd->args[n]));
 		n++;
 	}
-	//str_add(&cmd->args[0][n], "\0");
-	//printf("%ld\n", strlen(&cmd->args[0][n]));
 	return (0);
 }
 
 int	main(int argc,char **argv)
 {
 	t_command	*cmd;
-	int			i;
+	int			n_flag;
 	int			n;
 	int			to_ret;
 
 	cmd = (t_command *)malloc(sizeof(t_command));
 	if (!cmd)
 		return (1);
-	i = 0;
-	n = 0;
+	n_flag = 0;
+	n = 1;
 	to_ret = 0;
 	echo_aux(argc, argv, cmd);
 	if (!ft_strncmp(cmd->args[0], "echo", 4) && argc > 1)
 	{
 		if (!ft_strncmp(cmd->args[1], "-n", 3))
+			n_flag = 1;
+		while (n + n_flag < argc)
 		{
-			put_str_fd(cmd->args[2], 1);
-			write(1, "\%", 2);
-			to_ret = 0;
-		}
-		else
-		{
-			put_str_fd(cmd->args[1], 1);
-			write(1, "\n", 1);
+			put_str_fd(cmd->args[n + n_flag], 1);
+			n++;
+			if (n + n_flag <= argc -2)
+				write(1, " ", 2);
 		}
 	}
-	if (argc == 1)
+	if (!n_flag)
 		write(1, "\n", 1);
 	return (to_ret);
 }
