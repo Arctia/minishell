52d6d81c403678feab66fcc65b636d71efa19796
Vincenzo Gavioli <vgavioli@e4r10p1.42roma.it>
2023-02-27 14:16:43 +0100

    Master: header structs definitions

---
+++ global.h
@@ -0,0 +1,42 @@
+#ifndef GLOBAL_H
+# define GLOBAL_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <signal.h>
+# include <termios.h>
+# include <sys/wait.h>
+# include <sys/types.h>
+# include <readline/history.h>
+# include <readline/readline.h>
+
+// Constant declarations
+# define NAME_MAX 255
+# define PATH_MAX 1024
+
+// Struct declarations --> Every array/matrix should be null terminated
+typedef struct s_hellmini
+{
+	char 		**env;
+
+	int			*exit_status;
+	t_command 	*cmd;
+
+
+    pid_t 		pid; // maybe 
+}           t_hellmini;
+
+typedef struct s_command
+{
+	char	*command;
+	char	**flags;
+	char	**arguments;
+
+	int		ret;
+
+	t_command *next;
+	t_command *prev;
+
+	t_hellmini *shell;
+}	t_command;
