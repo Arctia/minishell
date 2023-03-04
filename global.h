981c9d61ca407e70c6c5104b3648f99455b0b04e
arctia <shadowwind@live.it>
2023-03-04 10:43:44 +0100

    new parameters in header structs

--- global.h
+++ global.h
@@ -1,6 +1,8 @@
 #ifndef GLOBAL_H
 # define GLOBAL_H
 
+// e libft?
+# include "./libft/libft.h"
 # include <stdio.h>
 # include <unistd.h>
 # include <stdlib.h>
@@ -18,25 +20,49 @@
 // Struct declarations --> Every array/matrix should be null terminated
 typedef struct s_hellmini
 {
-	char 		**env;
+	char 				**env;
 
-	int			*exit_status;
-	t_command 	*cmd;
+	int					exit_status;
+	char				*input;
+	struct s_command 	*current_cmd;
+	int					mc_pipes;
+	int					mc_quotes;
+	int					mc_wquotes;
 
-
-    pid_t 		pid; // maybe 
+//	pid_t				pid; // maybe
 }           t_hellmini;
 
+//words_operators e'la prima splittata(" ")dell'input, ergo la prima tokenizzazione;
+//potrebbe servirne una ulteriore per semplificare la lettura del codice(in generale ma
+//soprattutto in presenza di < > << >>)
 typedef struct s_command
 {
-	char	*command;
-	char	**flags;
-	char	**arguments;
+	char				**words_operators;
+	char				*command;
+	char				**flags;
+	char				**arguments;
 
 	int		ret;
 
-	t_command *next;
-	t_command *prev;
+	struct s_command 	*next;
+	struct s_command 	*prev;
 
-	t_hellmini *shell;
+	t_hellmini 			*shell;
 }	t_command;
+
+//tokenizer.c
+//int						tkn_nbr(char *str);
+//int						ft_strchr_len(char *str, char c);
+//t_command				* count_pqwq(t_command *command, char *str);
+//char					**rip_and_tear(t_command *command, char *str);
+
+//expander.c
+//void					free_arr(char **split_arr);
+//char					**ft_arrdup(char **arr);
+//char					*exp_tkn(char *str, char **env);
+
+//env_handlers.c
+//void					init_shell_env(char **pr_env, t_hellmini shell);
+
+
+#endif
