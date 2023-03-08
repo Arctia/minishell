c95a5cf05d782c54ea4792f56b676d45990b82fb
Vincenzo Gavioli <vgavioli@e4r10p2.42roma.it>
2023-03-08 16:38:29 +0100

    Parser: cleaning and using new libft functions. Still need to fill spc{}

--- parser/new_parser.c
+++ parser/new_parser.c
@@ -3,35 +3,36 @@
 
 /*##############################################################################
 #	Help Functions
-##############################################################################*/
+############################################################################*/
 
 int	until_space(const char *str)
 {
 	int	i;
 
 	i = 0;
-	while (str[i] && ft_is_not_space(str[i]))
+	while (str[i] && ft_isnotspace(str[i]))
 		i++;
 	return (i);
 }
 
 void	move_to_next_char(char *str)
 {
-	while (str[0] && ft_is_space(str[0]))
+	while (str[0] && ft_isspace(str[0]))
 		str++;
 }
 
 /*##############################################################################
 #	Set command name
-##############################################################################*/
+############################################################################*/
 static void	set_command_name(t_command *cmd)
 {
 	int	i;
 
 	i = 0;
-	cmd->command = (char *) malloc(sizeof(char) * ft_strlen(cmd->split[0]) + 1);
-	while (cmd->split[0][i++])
-		cmd->command[i - 1] = cmd->split[0][i - 1];
+	cmd->command = (char *) malloc(sizeof(char)
+			* ft_strlen(cmd->tokens[0]) + 1);
+	while (cmd->tokens[0][i++])
+		cmd->command[i - 1] = cmd->tokens[0][i - 1];
 	cmd->command[i] = '\0';
 }
 
@@ -46,16 +47,16 @@ static void	set_arguments(t_command *cmd, int args)
 	i = 1;
 	while (i < args)
 	{
-		cmd->arguments[i - 1] = (char *) malloc(sizeof(char) *
-			ft_strlen(cmd->split[i]) + 1);
+		cmd->arguments[i - 1] = (char *) malloc(sizeof(char)
+				* ft_strlen(cmd->tokens[i]) + 1);
 		if (cmd->arguments[i - 1])
 		{
 			free(cmd->arguments);
 			return ;
 		}
 		j = 0;
-		while (cmd->split[i][j++])
-			cmd->arguments[i - 1][j - 1] = cmd->split[i][j - 1];
+		while (cmd->tokens[i][j++])
+			cmd->arguments[i - 1][j - 1] = cmd->tokens[i][j - 1];
 		cmd->arguments[i - 1][j] = '\0';
 		i++;
 	}
@@ -63,12 +64,10 @@ static void	set_arguments(t_command *cmd, int args)
 
 /*##############################################################################
 #	Splitter Functions
-##############################################################################*/
-
-
+############################################################################*/
 /* __ items in string ___________________?______________________________________
 		. return number of items to be stored
-_________________________________________!___________________________________ */
+_________________________________________!_________________________________ */
 int	items_in_string(char *str)
 {
 	int	items_number;
@@ -80,7 +79,7 @@ int	items_in_string(char *str)
 	items_number = 0;
 	while (str[i])
 	{
-		if (ft_is_not_space(str[i]) && !in_word)
+		if (ft_isnotspace(str[i]) && !in_word)
 		{
 			in_word = 1;
 			items_number++;
@@ -95,7 +94,7 @@ int	items_in_string(char *str)
 /* __ Replace string ____________________?______________________________________
 		- cycle until space found
 		- inner cycle for quote/s (do not consider inside different quote)
-_________________________________________!___________________________________ */
+_________________________________________!_________________________________ */
 void	write_word_expander(char *cnt, char *str)
 {
 	char	fchar;
@@ -103,7 +102,7 @@ void	write_word_expander(char *cnt, char *str)
 
 	i = 0;
 	fchar = str[0];
-	while (ft_is_not_space(str[0]))
+	while (ft_isnotspace(str[0]))
 	{
 		fchar = str[0];
 		if ((fchar == '\'' || fchar == '\"') && str++[0])
@@ -111,7 +110,7 @@ void	write_word_expander(char *cnt, char *str)
 			while (str[0] && str[0] != fchar)
 				cnt[i++] = str++[0];
 		}
-		else 
+		else
 			cnt[i++] = str++[0];
 	}
 	cnt[i] = '\0';
@@ -122,7 +121,7 @@ void	write_word(char *cnt, char *str)
 	int	i;
 
 	i = 0;
-	while (ft_is_not_space(str[0]))
+	while (ft_isnotspace(str[0]))
 		cnt[i++] = str++[0];
 	cnt[i] = '\0';
 }
@@ -131,30 +130,30 @@ void	write_word(char *cnt, char *str)
 		. Count items to be store and allocate them
 		. Allocate every single item
 		. Write them
-_________________________________________!___________________________________ */
+_________________________________________!_________________________________ */
 int	split_string(t_command *cmd)
 {
 	char	*init;
 	int		items;
-	int 	c;
+	int		c;
 
 	init = cmd->str;
 	move_to_next_char(cmd->str);
 	items = items_in_string(cmd->str);
-	cmd->split = (char **) malloc(sizeof(char *) * items + 1);
-	if (!(cmd->split))
+	cmd->tokens = (char **) malloc(sizeof(char *) * items + 1);
+	if (!(cmd->tokens))
 		return (0);
 	c = 0;
 	while (cmd->str[0])
 	{
-		cmd->split[c] = (char *) malloc(sizeof(char) *
-			until_space(cmd->str) + 1);
-		if (!(cmd->split[c]))
+		cmd->tokens[c] = (char *) malloc(sizeof(char)
+				* until_space(cmd->str) + 1);
+		if (!(cmd->tokens[c]))
 			return (0);
-		write_word(cmd->split[c], cmd->str);
+		write_word(cmd->tokens[c], cmd->str);
 		c++;
 	}
-	cmd->split[c] = NULL;
+	cmd->tokens[c] = NULL;
 	cmd->str = init;
 	return (items);
 }
@@ -162,31 +161,27 @@ int	split_string(t_command *cmd)
 /*##############################################################################
 #	Tokenizer receives shell struct, cycles through all commands and
 		fill their fields. Every command struct should have a
-		str, which stores input with the operator at the end.
-##############################################################################*/
-
+		str, which stores input with the operator at the end
+############################################################################*/
 int	parser(t_hellmini *sh)
 {
 	t_command	*cmd;
 	int			args;
 
 	cmd = sh->current_cmd;
-	// tokenizer
 	while (cmd)
 	{
 		args = split_string(cmd);
 		if (!args)
 			return (FAIL);
 		set_command_name(cmd);
-		//set_flags(&cmd);
 		set_arguments(cmd, args);
 		cmd = cmd->next;
 	}
-	// actual parser
 	return (SUCCESS);
 }
 
-int	main(){
+int	main(void){
 	printf("test");
 	return (0);
 }
