9335e41867bd595826c9ab078731453269deeefb
arctia <arctia.games@gmx.com>
2023-03-11 13:56:21 +0100

    Parser: prepare merge

--- global.h
+++ global.h
@@ -8,10 +8,15 @@
 # include <stdlib.h>
 # include <signal.h>
 # include <termios.h>
+# include <sys/ioctl.h>
 # include <sys/wait.h>
 # include <sys/types.h>
 # include <readline/history.h>
 # include <readline/readline.h>
+
+//per far girare sigaction
+//#define _POSIX_C_SOURCE 199309L
+#define _POSIX_C_SOURCE 200809L
 
 // Constant declarations
 # define NAME_MAX 255
@@ -26,6 +31,12 @@
 # define REDOUT 5
 # define REDAPP 6
 # define HERDOC 7
+# define CASH 8
+
+# define PROMPT "\033[1;31mminisHELL$:\033[0m "
+
+//per far funzionare rl_replace_lline
+extern void	rl_replace_line(const char *text, int clear_undo);
 
 // Struct declarations --> Every array/matrix should be null terminated
 typedef struct s_hellmini
@@ -47,34 +58,50 @@ typedef struct s_hellmini
 //soprattutto in presenza di < > << >>)
 typedef struct s_command
 {
-	char				**tokens;
 	char				*str;
-	int					spc[8];
+	char				**tokens;
+	int					spc[9];
 
 	char				*command;
-	char				**flags;
+	char				**flags; // useless (?)
 	char				**arguments;
 
 	int		ret;
 
 	struct s_command 	*next;
 	struct s_command 	*prev;
+
 	t_hellmini 			*shell;
 }	t_command;
 
+
+/*lexer.c
+			lexer.c			*/
+int     				lexer_init(t_hellmini *shell);
+/*			lexer_splitter.c  */
+void					lexer_error(char *message);
+char					*split_operator(char *line, int *ff, int not_new);
+char					*split_line(char *line);
+
 //tokenizer.c
-//int						tkn_nbr(char *str);
-//int						ft_strchr_len(char *str, char c);
-//t_command				* count_pqwq(t_command *command, char *str);
-//char					**rip_and_tear(t_command *command, char *str);
+int						tkn_nbr(char *str);
+int						ft_strchr_len(char *str, char c);
+t_command				* count_pqwq(t_command *command, char *str);
+char					**rip_and_tear(t_command *command, char *str);
 
 //expander.c
-//void					free_arr(char **split_arr);
-//char					**ft_arrdup(char **arr);
-//char					*exp_tkn(char *str, char **env);
+void					free_arr(char **split_arr);
+char					**ft_arrdup(char **arr);
+char					*exp_tkn(char *str, char **env);
 
 //env_handlers.c
-//void					init_shell_env(char **pr_env, t_hellmini shell);
+void					init_shell_env(char **pr_env, t_hellmini shell);
 
-
+//signals.c
+void					ft_suppress_output(void);
+void					sigint_handler(int sig);
+void					sigquit_handler(int sig);
+void					sigquit_macro(int sig, t_hellmini *shell);
+void					*ft_handler(int sig);
+void					ft_sigs_handler(int sig);
 #endif
